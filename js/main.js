f4ba20b3fdf63321a6568c76244d2653a8e3dade
Antonio <antonioru1997@gmail.com>
2021-09-09 13:27:23 +0200

    Fine logica

--- js/main.js
+++ js/main.js
@@ -4,10 +4,28 @@ const app = new Vue(
     {
         el: "#root",
         data: {
-
+            newToDo: "",
+            toDos: [
+                "Fare i compiti",
+                "Fare la spesa",
+                "Fare il bucato"
+            ]
         },
         methods: {
-            
+            /* funzione per aggiungere alla lista */
+            addToDo() {
+                if (this.newToDo != "") {
+                    this.toDos.push(this.newToDo);
+                    this.newToDo = "";
+                } else {
+                    /* Messaggio nel caso in cui il campo inserito sia vuoto */
+                    alert("Inserire almeno 1 carattere");
+                }
+            },
+            /* Funzione per rimuovere dalla lista */
+            removeToDo(index) {
+                this.toDos.splice(index,1);
+            }
         }
     }
 )
